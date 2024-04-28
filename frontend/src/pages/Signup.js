90faaba6012ab6f427423431cd60fa04f282831a
Nikhil1O1 <technikue2.0@gmail.com>
2024-04-29 02:59:55 +0530

    added ui changes
    this one consit of singup page cosmetic enancement.
    In the previous commit i built backend server (basic) and bootstrapped pages with minor changes

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import {Col, Row, Container, Button, Form } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-import './Login.css';
+import './Signup.css';
+import pf from '../assets/pf.jpg';
 
 function Signup() {
   return (
@@ -8,6 +10,19 @@ function Signup() {
       <Row>
         <Col md ={7} className='d-flex align-items-center justify-content-center flex-direction-column' > 
         <Form style={{width: '80%', maxWidth: 500}}>
+          <h1>Create Account</h1>
+          <div className='signup-profile-pic__container'>
+            <img scr = {pf} className='signup-profile-pic'/>
+            <label htmlFor="image-upload" className='image-upload-label'></label>
+            <i className='fas fa-plus-circle add-picture-icon'></i>
+          </div>
+        <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Your Name" />
+        <Form.Text className="text-muted">
+          Something that you will like to be called.
+        </Form.Text>
+      </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
@@ -20,21 +35,22 @@ function Signup() {
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
       <Button variant="primary" type="submit">
         Signup
       </Button>
       <div className='py-4'>
         <p className='text-center'>
-          Already have an account ? <Link to="/login">Signup</Link>
+          Already have an account ? <Link to="/login">Login</Link>
         </p>
-
       </div>
     </Form>
         </Col>
-        <Col md = {5} className="login__bg"></Col>
-
+        <Col md = {5} className="signup__bg"></Col>
       </Row>
-    
     </Container>
   );
 }
